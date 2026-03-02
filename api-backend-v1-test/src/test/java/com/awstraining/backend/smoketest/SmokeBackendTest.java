ffa17fa4bf5702adf14bfb9ffb2850db204894dd
HubertKowalczykCapgemini <hubert.kowalczyk@capgemini.com>
2026-03-02 14:21:25 +0100

    delete test cases

--- api-backend-v1-test/src/test/java/com/awstraining/backend/smoketest/SmokeBackendTest.java
+++ api-backend-v1-test/src/test/java/com/awstraining/backend/smoketest/SmokeBackendTest.java
@@ -38,75 +38,9 @@ class SmokeBackendTest {
     }
 
 
-  @Test
-    void smokeTest_allCurlRequests() throws Exception {
-
-        // ================
-        // 1. curl http://LB
-        // ================
-        String rootResponse = apiClient.invokeAPI(
-                "/",                     // path
-                "GET",                   // method
-                new HashMap<>(),         // query params
-                null,                    // body
-                new HashMap<>(),         // headers
-                new HashMap<>(),         // cookies
-                new HashMap<>(),         // form params
-                "application/json",      // accept
-                null,                    // contentType
-                new String[]{}           // authNames
-        ).toString();
-
-        assertNotNull(rootResponse);
-        System.out.println("Root response: " + rootResponse);
-
-
-
-        // ======================================
-        // 2. curl GET /device/v1/test z basicAuth
-        // ======================================
-
-        String getResponse = apiClient.invokeAPI(
-                "/device/v1/test",
-                "GET",
-                new HashMap<>(),
-                null,
-                new HashMap<>(),
-                new HashMap<>(),
-                new HashMap<>(),
-                "application/json",
-                null,
-                new String[]{}       // Basic Auth podany globalnie (user/pass)
-        ).toString();
-
-        assertNotNull(getResponse);
-        System.out.println("GET /device/v1/test: " + getResponse);
-
-
-
-        // ======================================
-        // 3. curl POST /device/v1/test z JSON body
-        // ======================================
-
-        Map<String, Object> jsonBody = new HashMap<>();
-        jsonBody.put("type", "test");
-        jsonBody.put("value", -510.190);
-
-        String postResponse = apiClient.invokeAPI(
-                "/device/v1/test",
-                "POST",
-                new HashMap<>(),
-                jsonBody,                     // BODY
-                Map.of("Content-Type", "application/json"),
-                new HashMap<>(),
-                new HashMap<>(),
-                "application/json",
-                "application/json",
-                new String[]{}               // basicAuth przez user/pass
-        ).toString();
-
-        assertNotNull(postResponse);
-        System.out.println("POST /device/v1/test: " + postResponse);
+      @Test
+    void testSomething() {
+        // <<TODO: test something>>
     }
 
 }
